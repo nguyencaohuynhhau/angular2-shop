137ae5e039ef257b24f8f875c4a86f48951b8e20
DESKTOP-O3685UU\ADMIN <wwwillrockyou06@gmail.com>
2021-04-13 17:31:01 +0700

    done!

---
+++ src/app/core/common/system.constants.ts
@@ -0,0 +1,6 @@
+export class SystemConstants {
+    public static CURRENT_USER = "CURRENT_USER";
+    public static BASE_API = "http://simpleman.somee.com";
+    //public static BASE_API = "http://localhost:5000"
+
+}
